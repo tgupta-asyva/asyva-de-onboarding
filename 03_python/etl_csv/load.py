39e8a99cdaa8dc590bb7387779691d350d1fe85e
rahulasyva <rahul.pal@asyvainfotech.com>
2026-02-19 19:11:40 +0530

    load

--- 03_python/etl_csv/load.py
+++ 03_python/etl_csv/load.py
@@ -1,3 +1,13 @@
 """ load.py
 Fill in implementation.
 """
+def data_load(data, output_file):
+  try:
+    data.to_parquet(output_file, index = False)
+    print(f"Data load successfull to {output_file}.")
+  except Exception as e:
+    print(f"Error in data load:{e}")
+
+## loading the transformed data to a new file
+## output_file = "datasets/titanic_transformed.parquet"
+## load_data = data_load(transform_data, output_file)
